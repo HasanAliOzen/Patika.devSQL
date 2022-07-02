759f30a67a8c1a3a84e6205a2ba25c610ff3dc0d
HasanAliOzen <hasanali.ozen@outlook.com>
2022-07-02 21:31:15 +0300

    Patika.dev hw 9

---
+++ hw9.sql
@@ -0,0 +1,18 @@
+-- Patika.dev SQL Homework 9
+
+
+-- city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT city, country FROM city
+INNER JOIN country ON city.country_id = country.country_id;
+
+
+-- customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name
+-- isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT payment_id, first_name, last_name FROM payment
+INNER JOIN customer ON payment.customer_id = customer.customer_id;
+
+
+--customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name
+-- isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT rental_id, first_name, last_name FROM rental
+INNER JOIN customer ON rental.customer_id = customer.customer_id;
